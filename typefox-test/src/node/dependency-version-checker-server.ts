d53824eef7c3a479e7084b148f83d4cfbb533c14
Jonah Iden <jonah.iden@idl.eu>
2022-08-30 22:49:20 +0200

    basic functionality working

--- typefox-test/src/node/dependency-version-checker-server.ts
+++ typefox-test/src/node/dependency-version-checker-server.ts
@@ -2,22 +2,74 @@ import { JsonRpcServer, logger, Logger } from "@theia/core";
 import { FileSearchService } from "@theia/file-search/lib/common/file-search-service";
 import { inject, injectable } from "@theia/core/shared/inversify";
 import { IDependencyVersionCheckServer } from "../common/dependency-version-checker-protocoll";
+import * as fs from 'fs';
+import * as toml from 'toml'
+import { FileUri } from '@theia/core/lib/node/file-uri';
 
 
 @injectable()
 export class DependencyVersionCheckerServer implements IDependencyVersionCheckServer {
+    
     constructor(@inject(FileSearchService) private fileSearchService: FileSearchService) {}
 
     /**
      * analyzes the dependencies from the cargo.toml and returns a list of mismatches
      */
     async analyzeDependencies(): Promise<string[]> {
-        console.log("called analyze dependencies")
-        const cargoTomlFiles: string[] = await this.fileSearchService.find("cargo.toml", {});
-        console.log(cargoTomlFiles);
+        // this path is hardcoded right now for my local test workspace because i couldn't figure out where to get the acutal path from
+        const cargoTomlFiles: string[] = await this.fileSearchService.find("cargo.toml", {rootUris: ["d:/Projekte/Kleinere_Projekte/Rust"]});
+        const allDependencies: Dependency[] = []
+        for(let path of cargoTomlFiles) {
+            const tomlData = toml.parse(await this.getFileContent(path));
+            Object.entries(tomlData.dependencies).forEach((key,value) => {
+                console.log(key[0] + " " + key[1])
+                allDependencies.push({name: key[0], version: "" + key[1]})
+            });
+        }
 
-        return ["test 1", "test 2"];
+
+        return this.validateDependencyVersions(allDependencies);
     }
+
+    private async getFileContent(path: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            fs.readFile(new URL(decodeURIComponent(path)), "utf8", (err, data) => {
+                if(err) {
+                    console.log("error reading file " + err) // needs of course better error handling when fully developed
+                    reject("error reading file")
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    }
+
+    /**
+     * goes through the given dependencies and checks for duplicates with different version
+     * returns mismatches
+     * @param dependencies the dependencies to check
+     */
+    private validateDependencyVersions(dependencies: Dependency[]): string[] {
+        const searchMap = new Map<string, string>();
+        const mismatches = [];
+        for(let dep of dependencies) {
+            if(searchMap.has(dep.name)) {
+                if(dep.version != searchMap.get(dep.name)) {
+                    mismatches.push("mismatch in " + dep.name + " versions: " + dep.version + "!=" + searchMap.get(dep.name))
+                }
+            } else {
+                searchMap.set(dep.name, dep.version);
+            }
+        }
+        return mismatches;
+    }
+
+
 }
 
+
+interface Dependency {
+    name: string;
+    version: string;
+} 
 
